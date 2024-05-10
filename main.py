0adc77383596ad7706ded1e34833d3ad2d8921d3
guar1337 <bogumil.zalewski@gmail.com>
2024-05-10 22:33:35 +0200

    Opening pre cleaned .root files with uproot accessing arrays with strips readout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import uproot
+import pandas as pd
+import csv
+import json
 
+
+# testing for one file
 file = uproot.open("C:\\Users\\zalewski\\Desktop\\work\\data\\geo1\\cln_run01.root:cleaned")
 # print(file.keys())
 df = file.arrays(['SQX_L', 'SQY_L', 'CsI_L',
@@ -8,6 +13,30 @@ df = file.arrays(['SQX_L', 'SQY_L', 'CsI_L',
                   'F3', 'tdcF3', 'F5', 'tdcF5',
                   'nx1', 'ny1', 'nx2', 'ny2',
                   'x1', 'y1', 'x2', 'y2', 'trigger'], library="pd")
+
+data = {}
+with open('constants.json', 'r') as fp:
+    data = json.load(fp)
+
+# read column + calibration path pair
+for column, path in data.items():
+    slope = []
+    intercept = []
+    pixel_number = 0
+    # open calibration parameters file for each detector
+    with open(path, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        # read calibration parameters for each pixel
+        for row in reader:
+            slope.append(float(row[0]))
+            intercept.append(float(row[1]))
+            df[column] = df[column].apply(lambda x: slope[pixel_number] * x + intercept[pixel_number])
+            pixel_number += 1
+
+
+
+
+
 
 df['strip_X_L'] = df['SQX_L'].apply(lambda x: np.argmax(x))
 df['strip_Y_L'] = df['SQY_L'].apply(lambda x: np.argmax(x))
@@ -21,5 +50,13 @@ df['sqrde'] = df['SQX_R'].apply(lambda x: np.max(x))
 df['sqletot'] = df['CsI_L'].apply(lambda x: np.max(x))
 df['sqretot'] = df['CsI_R'].apply(lambda x: np.max(x))
 
+df['tF3'] = df['tdcF3'].apply(lambda x: np.mean(x))
+df['tF5'] = df['tdcF5'].apply(lambda x: np.mean(x))
+df['aF5'] = df['F5'].apply(lambda x: np.mean(x))
+
 df.drop(['SQX_L', 'SQY_L', 'CsI_L',
-         'SQX_R', 'SQY_R', 'CsI_R'], axis=1, inplace=True)
+         'SQX_R', 'SQY_R', 'CsI_R',
+         'tdcF3', 'tdcF5', 'F5', 'F3'], axis=1, inplace=True)
+
+
+
